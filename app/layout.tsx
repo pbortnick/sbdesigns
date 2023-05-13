5230c3a01dbe7626104540c549e435ab55a6893c
pbortnick <pbortnick@gmail.com>
2023-05-13 16:44:31 -0400

    fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,25 @@ import './globals.css'
 import { inter, k2d, josefinSans } from 'lib/fonts'
 import Head from 'next/head'
 import Footer from '@/components/footer'
+import { type Metadata } from 'next'
 
-export const metadata = {
-  title: 'Sb Designs',
+export const metadata: Metadata = {
+  title: 'SB Designs',
   description: `I work to have an understanding of my clients' style preferences which I use to create an environment where they feel happy, comfortable, and, most importantly, at home.`,
+  openGraph: {
+    images: ['https://www.sbdesigns516.com/og-image.png'],
+  },
+  twitter: {
+    images: ['https://www.sbdesigns516.com/og-image.png'],
+  },
+  icons: {
+    icon: '/favicon.svg',
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
 }
 
 export default function RootLayout({
@@ -15,14 +30,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.svg" type="image/png" sizes="32x32" />
-        <meta
-          property="og:image"
-          content="https://www.sbdesigns516.com/og-image.png"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <body
         className={`${inter.variable} ${k2d.variable} ${josefinSans.variable}`}
       >
